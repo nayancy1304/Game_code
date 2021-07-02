5249fdc8185656379a51aac13e65d22722928c19
nayancy1304 <75105326+nayancy1304@users.noreply.github.com>
2021-07-02 18:34:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,47 @@
-var box 
+ 
 function setup() {
   createCanvas(800,600);
-  box = createSprite(200,200,300,100);
-  box.shapeColor = "Yellow"
 }
 
 function draw() 
 {
-  background("skyblue");
-  fill("red");
-  rect (400,300,50,50);
-  fill("green");
-  ellipse (250,400,100,100);
+  background("yellow");
   fill("blue");
   textFont("Georgia");
   textSize("20");
-  text("Different shapes",250,20);
+  text("Different background colors",250,20);
   triangle (300,300,350,300,325,250,300,300)
   if (keyIsDown(RIGHT_ARROW)) {
-    box.x += 4;
+    background("cyan");
+    fill("blue");
+    textFont("Georgia");
+    textSize("20");
+    text("Different background colors",250,20);
+    triangle (300,300,350,300,325,250,300,300)
   }
   if (keyIsDown(LEFT_ARROW)){
-    box.x -= 4;
+    background("purple");
+    fill("blue");
+    textFont("Georgia");
+    textSize("20");
+    text("Different background colors",250,20);
+    triangle (300,300,350,300,325,250,300,300)
   }
   if (keyIsDown(UP_ARROW)){
-    box.y -= 4;
+    background("red")
+    fill("blue");
+    textFont("Georgia");
+    textSize("20");
+    text("Different background colors",250,20);
+    triangle (300,300,350,300,325,250,300,300)
     }
   if (keyIsDown(DOWN_ARROW)) {
-    box.y += 4;
+    background("green");
+    fill("blue");
+    textFont("Georgia");
+    textSize("20");
+    text("Different background colors",250,20);
+    triangle (300,300,350,300,325,250,300,300);
   }
   drawSprites()
 }
