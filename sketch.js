d4ffa259719f5189a9220de42a73f55cdaf5cd7d
nayancy1304 <75105326+nayancy1304@users.noreply.github.com>
2021-07-08 13:05:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,31 @@
- 
-function setup() {
-  createCanvas(800,600);
+var seaImage, ship1, ship;
+function preload(){
+//loading animation and image
+ship1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+seaImage = loadImage("sea.png");
 }
 
-function draw() 
-{
-  background("yellow");
-  fill("blue");
-  textFont("Georgia");
-  textSize("20");
-  text("Different background colors",250,20);
-  triangle (300,300,350,300,325,250,300,300)
-  if (keyIsDown(RIGHT_ARROW)) {
-    background("cyan");
-    fill("blue");
-    textFont("Georgia");
-    textSize("20");
-    text("Different background colors",250,20);
-    triangle (300,300,350,300,325,250,300,300)
+function setup(){
+  createCanvas(700,600);
+  //creating sea and the ship
+  sea = createSprite(700,300)
+  sea.addImage("Sea",seaImage);
+  //giving velocity to the sea
+  sea.velocityX = -2
+  sea.x = sea.width/2
+  ship = createSprite(200,400,30,30)
+  ship.addAnimation("Ship",ship1);
+  ship.scale = 0.5 
+}
+
+function draw() {
+  background("white");
+  //giving velocity to the ship
+  sea.velocityX = -2
+  //condition for infinite sea
+  if (sea.x < 0) {
+    sea.x = sea.width/2
   }
-  if (keyIsDown(LEFT_ARROW)){
-    background("purple");
-    fill("blue");
-    textFont("Georgia");
-    textSize("20");
-    text("Different background colors",250,20);
-    triangle (300,300,350,300,325,250,300,300)
-  }
-  if (keyIsDown(UP_ARROW)){
-    background("red")
-    fill("blue");
-    textFont("Georgia");
-    textSize("20");
-    text("Different background colors",250,20);
-    triangle (300,300,350,300,325,250,300,300)
-    }
-  if (keyIsDown(DOWN_ARROW)) {
-    background("green");
-    fill("blue");
-    textFont("Georgia");
-    textSize("20");
-    text("Different background colors",250,20);
-    triangle (300,300,350,300,325,250,300,300);
-  }
+  ship.velocityX = 2
   drawSprites()
 }
